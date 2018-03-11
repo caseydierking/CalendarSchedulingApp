4bb3790d2cbb757fa242a190b4e78ac00a207252
MisterOptimist <casey@thebridgechurch.cc>
2018-03-10 18:37:07 -0600

    created login form with locale variations, english and french

---
+++ src/cd_calendarapplication/CD_CalendarApplication.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cd_calendarapplication;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author caseydierking
+ */
+public class CD_CalendarApplication extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+        //Locale.setDefault(new Locale("no", "NO"));
+        //Locale.setDefault(new Locale("no", "NO"));
+       // Locale.setDefault(new Locale("en", "US"));English - en_US
+
+
+		
+		
+        
+        
+    }
+    
+}
---
+++ src/cd_calendarapplication/FXMLDocumentController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cd_calendarapplication;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author caseydierking
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Label loginLabel;
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+      // This allows multiple languages to be supported for the login screen. Uncomment French and comment US to test.
+      //Locale.setDefault(new Locale("fr", "FR"));
+      
+      Locale.setDefault(new Locale("en", "US"));//English - en_US
+      ResourceBundle reb = ResourceBundle.getBundle("language_files/rb");
+	
+      loginLabel.setText(reb.getString("Login"));
+      usernameLabel.setText(reb.getString("username"));  
+      passwordLabel.setText(reb.getString("password"));
+      
+      
+        
+    }    
+    
+}
